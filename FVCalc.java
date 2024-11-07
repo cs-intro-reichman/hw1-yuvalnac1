8d0885706a9d4de4b92314bf762a0dd7bf7d1f52
Yuval Nachman <yuval.nachman01@post.runi.ac.il>
2024-11-07 19:38:57 +0200

    added 'a' to FVCalcs output

--- FVCalc.java
+++ FVCalc.java
@@ -5,7 +5,7 @@ public class FVCalc {
 		double rate = Double.parseDouble(args[1]);
 		int numOfYears = Integer.parseInt(args[2]);
 		double futureValue = currentValue * Math.pow(1 + (rate / 100),  numOfYears);
-		System.out.println("After " + numOfYears + " years, $" + currentValue +
+		System.out.println("After " + numOfYears + " years, a $" + currentValue +
 							" saved at " + rate + "% will yield $" + (int) futureValue);
 	}
 }
