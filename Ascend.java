7b480c82f8a6ae85eba1c7139b18e726e7581f69
Yuval Nachman <yuval@Yuvals-MacBook-Pro.local>
2024-11-07 17:42:07 +0200

    HW1 completed, first commit

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,14 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (Math.random() * lim);
+		int b = (int) (Math.random() * lim);
+		int c = (int) (Math.random() * lim);
+		System.out.println(a + " " + b + " " + c);
+		int minimum = Math.min(Math.min(a, b), c);
+		int maximum = Math.max(Math.max(a, b), c);
+		int middle = a + b + c - minimum - maximum;
+		System.out.println(minimum + " " + middle + " " + maximum);
 	}
 }
--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,11 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name2 = args[1];
+		String name3 = args[2];
+		double individualAmount = Math.ceil(Double.parseDouble(args[3]) / 3);
+		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + 
+							": pay " + individualAmount + " Shekels each.");
 	}
 }
